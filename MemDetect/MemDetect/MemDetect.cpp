e2ed6975f6765e8be1f495b9d644acc2eaefad87
coppx <fudaichao2133521@gmail.com>
2021-11-19 19:08:25 +0800

    add

--- MemDetect/MemDetect/MemDetect.cpp
+++ MemDetect/MemDetect/MemDetect.cpp
@@ -1,30 +1,76 @@
-#include "stackAllocator.h"
+#include <mutex>
 
-using std::size_t;
+#define FILENAME_SIZE 200
 
-struct new_ptr_list_t {
-	new_ptr_list_t* prev;
-	new_ptr_list_t* next;
+#ifndef DEBUG_ALIGNMENT
+#define DEBUG_ALIGNMENT 16
+#endif
+
+#define ALIGN(s) (((s) + DEBUG_ALIGNMENT - 1) & ~(DEBUG_ALIGNMENT - 1))
+
+struct ptr_list_item {
+	ptr_list_item()
+		: prev(nullptr)
+		, next(nullptr)
+		, size(0)
+		, line(0)
+		, is_array(false)
+	{
+
+	}
+
+	ptr_list_item* prev;
+	ptr_list_item* next;
 	size_t size;
 	union 
 	{
-		char file[200];
+		char file[FILENAME_SIZE];
 		void* addr;
 	};
 	size_t line;
 	bool is_array;
 };
 
+#define LIST_ITEM_SIZE  ALIGN(sizeof ptr_list_item)
+
 class MemDetect
 {
 public:
-	MemDetect();
-	~MemDetect();
+	//MemDetect();
+	//~MemDetect();
 	void* operator new(size_t size);
 	void operator delete(void *pdead, size_t size);
 
 	void* operator new[](size_t size);
 	void operator delete[](void* pdead, size_t size);
 private:
-	new_ptr_list_t* ptr_list;
+	void* allocate(size_t size, char* filename, int line, bool isArray);
+	void* deallocate();
+	ptr_list_item* list;
+	std::mutex m;
 };
+
+void* MemDetect::allocate(size_t size, char* filename, int line, bool isArray)
+{
+	int s = size + LIST_ITEM_SIZE;
+	ptr_list_item* raw_ptr = (ptr_list_item*)malloc(s);
+	if (nullptr == raw_ptr)
+	{
+		return nullptr;
+	}
+	std::lock_guard<std::mutex> lock(m);
+	if (nullptr == list)
+	{
+		list = raw_ptr;
+	}
+	raw_ptr->size = size;
+	raw_ptr->line = line;
+	raw_ptr->is_array = isArray;
+
+}
+
+int main()
+{
+	MemDetect d;
+	return 0;
+}
