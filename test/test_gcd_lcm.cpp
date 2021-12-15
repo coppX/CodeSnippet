51d85c471033782a14c9a0b5f8a9f47171db6e59
coppx <fudaichao2133521@gmail.com>
2021-12-15 13:31:50 +0800

    add gcd_lcm

---
+++ test/test_gcd_lcm.cpp
@@ -0,0 +1,86 @@
+//
+// Created by FDC on 2021/12/15.
+//
+
+#include "template_if_while.h"
+#include <iostream>
+
+int lcm(int a, int b)
+{
+    int r, lcm = a * b;
+    while (r = a % b)
+    {
+        a = b;
+        b = r;
+    }
+    return lcm/b;
+}
+
+int gcd_r(int a, int b)
+{
+    return b == 0 ? a :gcd_r(b, a % b);
+}
+
+int lcm_r(int a, int b)
+{
+    return a * b / gcd_r(a, b);
+}
+
+template<int a, int b>
+class lcm_T
+{
+    template<typename stat>
+    class cond
+    {
+    public:
+        enum {ret=(stat::div!=0)};
+    };
+
+    template <int a, int b>
+    class stat
+    {
+    public:
+        typedef stat<b, a % b> Next;
+        enum { div = a % b, ret = b};
+    };
+
+    static const int gcd = WHILE_<cond, stat<a, b>>::reType::ret;
+public:
+    static const int ret = a * b / gcd;
+};
+
+
+template<int a, int b>
+class lcm_T_r
+{
+    template<int a, int b>
+    class gcd
+    {
+    public:
+        enum {
+            ret = gcd<b, a % b>::ret
+        };
+    };
+
+    template<int a>
+    class gcd<a, 0>
+    {
+    public:
+        enum
+        {
+            ret = a
+        };
+    };
+public:
+    static const int ret = a * b / gcd<a, b>::ret;
+};
+
+int main()
+{
+    std::cout << lcm(100, 36) << '\n';
+    std::cout << lcm_r(100, 36) << '\n';
+    std::cout << lcm_T<100, 36>::ret << '\n';
+    std::cout << lcm_T_r<100, 36>::ret << '\n';
+    std::cin.get();
+    return 0;
+}
--- test/test_template_while.cpp
+++ test/test_template_while.cpp
@@ -4,28 +4,49 @@
 #include "template_if_while.h"
 #include <iostream>
 
-//计算1^e + 2^e + ... + n^e
-
-
 // 计算 1^e+2^e+...+n^e
 template<int n, int e>
-class sum_pow {
-    template<int i, int e> class pow_e{ public: enum{ ret=i*pow_e<i,e-1>::ret }; };
-    template<int i> class pow_e<i,0>{ public: enum{ ret=1 }; };
-// 计算 i^e，嵌套类使得能够定义嵌套模板元函数，private 访问控制隐藏实现细节
-    template<int i> class pow{ public: enum{ ret=pow_e<i,e>::ret }; };
+class sum_pow{
+
+    template<int i, int e>
+    class pow_e{
+    public:
+        enum{ ret = i * pow_e<i, e - 1>::ret };
+    };
+
+    template<int i>
+    class pow_e<i, 0>{
+    public:
+        enum{ ret = 1 };
+    };
+
+    // 计算 i^e，嵌套类使得能够定义嵌套模板元函数，private 访问控制隐藏实现细节
+    template<int i>
+    class pow{
+    public:
+        enum{ ret = pow_e<i, e>::ret };
+    };
+
     template<typename stat>
-    class cond { public: enum{ ret=(stat::ri<=n) }; };
+    class cond {
+    public:
+        enum{ ret = (stat::ri <= n) };
+    };
+
     template<int i, int sum>
-    class stat { public: typedef stat<i+1, sum+pow<i>::ret> Next;
-        enum{ ri=i, ret=sum }; };
+    class stat {
+    public:
+        typedef stat<i + 1, sum + pow<i>::ret> Next;
+        enum{ ri = i, ret = sum };
+    };
 public:
-    enum{ ret = WHILE_<cond, stat<1,0>>::reType::ret };
+    enum{ ret = WHILE_<cond, stat<1, 0>>::reType::ret };
 };
 
 int main()
 {
     std::cout << sum_pow<10, 2>::ret << '\n';
+
     std::cin.get();
     return 0;
 }
